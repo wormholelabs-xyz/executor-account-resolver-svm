08c23093f3631cf23e7a3ed719995ebaf4c86bb7
Evan Gray <battledingo@gmail.com>
2025-06-04 16:27:19 -0400

    custom discriminator

--- tests/solana-account-resolver.ts
+++ tests/solana-account-resolver.ts
@@ -1,31 +1,39 @@
+import { splDiscriminate } from "@solana/spl-type-length-value";
 import { AccountMeta } from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaAccountResolver } from "../target/types/solana_account_resolver";
+import { expect } from "chai";
 
 describe("solana-account-resolver", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.SolanaAccountResolver as Program<SolanaAccountResolver>;
+  const program = anchor.workspace
+    .SolanaAccountResolver as Program<SolanaAccountResolver>;
 
   const payer = anchor.web3.Keypair.generate();
   // airdrop to payer
   it("Is initialized!", async () => {
-    await anchor.getProvider().connection.confirmTransaction(
-      await anchor.getProvider().connection.requestAirdrop(payer.publicKey, 1000000000),
-      "confirmed"
-    );
+    await anchor
+      .getProvider()
+      .connection.confirmTransaction(
+        await anchor
+          .getProvider()
+          .connection.requestAirdrop(payer.publicKey, 1000000000),
+        "confirmed"
+      );
 
     // const [bar, _] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("bar"), Buffer.from([10])], program.programId);
     // const [bar, _] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("bar"), Buffer.from([10])], program.programId);
-    const tx = await program.methods.initialize().accounts(
-      {
+    const tx = await program.methods
+      .initialize()
+      .accounts({
         payer: program.provider.publicKey,
         // foo: fooKeypair.publicKey,
         // bar: bar
-      }
-    ).rpc();
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
     try {
       const result = await resolveInstructions(program);
@@ -38,48 +46,74 @@ describe("solana-account-resolver", () => {
           let payerConst = new anchor.web3.PublicKey(Array(32).fill(9));
 
           let tx = new anchor.web3.Transaction();
-          let accountsWithPayerOverride = instruction.accounts.map((account) => {
-            if (account.pubkey.equals(payerConst)) {
-              return { ...account, pubkey: payer.publicKey };
+          let accountsWithPayerOverride = instruction.accounts.map(
+            (account) => {
+              if (account.pubkey.equals(payerConst)) {
+                return { ...account, pubkey: payer.publicKey };
+              }
+              return account;
             }
-            return account;
-          });
-          tx.add(new anchor.web3.TransactionInstruction({
-            keys: accountsWithPayerOverride,
-            programId: instruction.programId,
-            data: instruction.data,
-          }));
-          let signature = await anchor.getProvider().connection.sendTransaction(tx, [payer]);
-          await anchor.getProvider().connection.confirmTransaction(signature, "confirmed");
+          );
+          tx.add(
+            new anchor.web3.TransactionInstruction({
+              keys: accountsWithPayerOverride,
+              programId: instruction.programId,
+              data: instruction.data,
+            })
+          );
+          let signature = await anchor
+            .getProvider()
+            .connection.sendTransaction(tx, [payer]);
+          await anchor
+            .getProvider()
+            .connection.confirmTransaction(signature, "confirmed");
         }
       }
     } catch (e) {
       console.log("Error", e);
     }
   });
+
+  it("derives the right discriminators", async () => {
+    const expectedBytes = Buffer.from([192, 102, 177, 206, 149, 99, 9, 192]);
+    const discriminator = await splDiscriminate(
+      "executor-account-resolver:relay-vaa-v1"
+    );
+    expect(expectedBytes).to.deep.equal(discriminator);
+    expect(
+      program.idl.instructions.find((x) => x.name === "accountsToExecute")
+        .discriminator
+    ).to.deep.equal([...expectedBytes]);
+  });
 });
 
 type Instruction = {
   programId: anchor.web3.PublicKey;
-  accounts: AccountMeta[],
-  data: Buffer,
-}
-
+  accounts: AccountMeta[];
+  data: Buffer;
+};
 
 // a function that calls accountsToExecute repeatedly until it returns ok. as
 // long as it returns missing, we add the returned missing keys to
 // remainingAccounts and call accountsToExecute again
-async function resolveInstructions(program: Program<SolanaAccountResolver>): Promise<Instruction[][]> {
+async function resolveInstructions(
+  program: Program<SolanaAccountResolver>
+): Promise<Instruction[][]> {
   let remainingAccounts: AccountMeta[] = [];
   let runs = 0;
   while (true) {
     runs++;
-    const result = await program.methods.accountsToExecute().remainingAccounts(remainingAccounts).view();
+    const result = await program.methods
+      .accountsToExecute()
+      .remainingAccounts(remainingAccounts)
+      .view();
     if (result.resolved) {
       console.log("Runs", runs);
       return result.resolved[0][0];
     } else {
-      let newAccountMetas = result.missing[0].map((key) => { return { pubkey: key, isSigner: false, isWritable: false } });
+      let newAccountMetas = result.missing[0].map((key) => {
+        return { pubkey: key, isSigner: false, isWritable: false };
+      });
       remainingAccounts.push(...newAccountMetas);
     }
   }
